f2ef22915e8d0ca7e938d9926cff82bdb6a74012
root <root@crawl.test>
2017-10-11 04:50:06 +0000

    Add script to run instagram tag scraper

---
+++ script/instagram_tag_crawl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+EPOCH=$(date +"%Y%m%d%H%M")
+
+echo Start crawling. Epoch is $EPOCH.
+
+mkdir /archd/archive/instagram_tag/$EPOCH
+
+# Crawl page.
+python /home/gjoliver/spiders/phantom/instagram_tag.py -l /home/gjoliver/urls/uid-list-instagram -k /home/gjoliver/insta_cookies -o /archd/archive/instagram_tag/$EPOCH
+
+# Scrape data and store in CloudSql.
+python /home/gjoliver/spiders/scrape/scrape/spiders/instagram_tag.py -i /archd/archive/instagram_tag/$EPOCH -o /home/j/data/INSTA_TAG/$EPOCH.csv
