b07cc634e3623b31a421689c8a5ed0cab775e42c
root <root@crawl.test>
2017-12-31 03:47:20 +0000

    Get all the accounts that have < 1000 posts.

--- scrape/scrape/pipelines.py
+++ scrape/scrape/pipelines.py
@@ -244,7 +244,7 @@ class SaveToCSVPipeline(object):
             # Write the special instagram picture to crawl file.
             with open('/home/j/data/insta_pics_tmp', 'a') as outfile:
                 for p in item['pics'][:5]:
-                    if item['posts'] > 500 or item['followers'] < 100000:
+                    if item['posts'] > 1000 or item['followers'] < 100000:
                         continue
 
                     outfile.write(item['uid'] + '\t' + p + '\n')
